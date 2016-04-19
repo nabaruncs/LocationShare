9e81509f4fcb7e19b62e23be41c2a5450bfd4a5d
Nabarun Banerjee <nabaruncs@yahoo.com>
2016-04-20 00:48:33 +0530

    add marker

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,7 +2,6 @@ var LOCATIONSHARE = LOCATIONSHARE || {};
 
 $(function() {
   OPENLAYER.initMap();
-  OPENLAYER.addMarker([45.8167, 15.9833]);
 });
 
 LOCATIONSHARE.PinLocations = {
@@ -14,95 +13,50 @@ LOCATIONSHARE.PinLocations = {
 };
 
 OPENLAYER = {
-  addMarker: function(location) {
-    var map = L.map('map').setView(location, 10);
-    var mbUrl = 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png';
-    L.tileLayer(mbUrl, {id: 'examples.map-i875mjb7'}).addTo(map);
-    var marker = L.marker(location).bindPopup("Zagreb").addTo(map);
-  },
-
   initMap: function() {
-    var iconFeature = new ol.Feature({
-      geometry: new ol.geom.Point(ol.proj.transform([15.9833, 45.8167], 'EPSG:4326', 'EPSG:3857')),
-      name: 'Zagreb',
+    var vectorSource = new ol.source.Vector({
+      //create empty vector
     });
 
+    //create a bunch of icons and add to source vector
+    for (var i=0;i<50;i++){
+
+        var iconFeature = new ol.Feature({
+          geometry: new
+            ol.geom.Point(ol.proj.transform([Math.random()*360-180, Math.random()*180-90], 'EPSG:4326',   'EPSG:3857')),
+        name: 'Null Island ' + i,
+        population: 4000,
+        rainfall: 500
+        });
+        vectorSource.addFeature(iconFeature);
+    }
+
+    //create the style
     var iconStyle = new ol.style.Style({
       image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
         anchor: [0.5, 46],
         anchorXUnits: 'fraction',
         anchorYUnits: 'pixels',
         opacity: 0.75,
-        src: 'http://ol3js.org/en/master/examples/data/icon.png'
+        src: 'http://openlayers.org/en/v3.9.0/examples/data/icon.png'
       }))
     });
-    iconFeature.setStyle(iconStyle);
 
-    var vectorSource = new ol.source.Vector({
-      features: [iconFeature]
-    });
 
+
+    //add the feature vector to the layer vector, and apply a style to whole layer
     var vectorLayer = new ol.layer.Vector({
-      source: vectorSource
+      source: vectorSource,
+      style: iconStyle
     });
 
     var map = new ol.Map({
+      layers: [new ol.layer.Tile({ source: new ol.source.OSM() }), vectorLayer],
       target: document.getElementById('map'),
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        }),
-        vectorLayer
-      ],
-      controls: ol.control.defaults(),
       view: new ol.View({
-        center: ol.proj.transform([15.9833, 45.8167], 'EPSG:4326', 'EPSG:3857'),
-        zoom: 10
+        center: [0, 0],
+        zoom: 3
       })
-    });
-
-    var element = document.getElementById('popup');
-    var popup = new ol.Overlay({
-      element: element,
-      positioning: 'bottom-center',
-      stopEvent: false
-    });
-    map.addOverlay(popup);
-
-    map.on('click', function(evt) {
-      var feature = map.forEachFeatureAtPixel(evt.pixel,
-                                              function(feature, layer) {
-                                                return feature;
-                                              });
-      if (feature) {
-        var geometry = feature.getGeometry();
-        var coord = geometry.$.get(url,
-                                   values,
-                                   function(data){
-
-                                   }
-                                  );Coordinates();
-        popup.setPosition(coord);
-        $(element).popover({
-          'placement': 'top',
-          'html': true,
-          'content': feature.get('name')
-        });
-        $(element).popover('show');
-      } else {
-        $(element).popover('destroy');
-      }
-    });
-
-    map.on('pointermove', function(e) {
-      if (e.dragging) {
-        $(element).popover('destroy');
-        return;
-      }
-
-      var pixel = map.getEventPixel(e.originalEvent);
-      var hit = map.hasFeatureAtPixel(pixel);
-      map.getTarget().style.cursor = hit ? 'pointer' : '';
     });
   }
 }
